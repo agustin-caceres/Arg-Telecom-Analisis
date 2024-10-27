624f3a588c95a3d31a53f4cfd793115d8f90a0a8
Agustín Cáceres <agusca.saot@gmail.com>
2024-10-27 05:37:37 -0300

    Se agrega app de Streamlit

---
+++ Dashboard_Streamlit/paginas/kpi_1.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from data_loader import load_internet_penetration_data, cargar_datos_penetracion_mapa
+from visualization import graficar_penetracion_internet, graficar_comparativa_acceso_proyectado, graficar_evolucion_penetracion_provincia, graficar_mapa_penetracion
+
+def display():
+    st.title("KPI 1 - Acceso a Internet")
+
+    # Cargar los datos de penetración general y mapa
+    df_penetracion = load_internet_penetration_data()
+    df_penetracion_mapa = cargar_datos_penetracion_mapa()
+    geojson_path = 'Dashboard_Streamlit/ProvinciasArgentina_actualizado.geojson'
+
+    # Mostrar gráficos iniciales
+    st.subheader("Penetración de Internet por 100 Hogares (2023 en adelante)")
+    graficar_penetracion_internet(df_penetracion)
+
+    st.subheader("Comparativa de Acceso a Internet: Actual vs. Proyectado (2% Aumento)")
+    graficar_comparativa_acceso_proyectado(df_penetracion)
+
+    # Subtítulo de la sección de evolución y filtro de selección
+    st.subheader("Evolución de la Penetración por Provincia")
+
+    # Sidebar para seleccionar la provincia
+    provincia_seleccionada = st.selectbox(
+        'Selecciona una provincia:', 
+        sorted(df_penetracion[df_penetracion['anio'] >= 2023]['nombre_provincia'].unique())
+    )
+
+    # Gráfico de evolución de penetración de internet por provincia
+    graficar_evolucion_penetracion_provincia(df_penetracion, provincia_seleccionada)
+
+    # Mapa de penetración por provincia
+    graficar_mapa_penetracion(df_penetracion_mapa, geojson_path)
